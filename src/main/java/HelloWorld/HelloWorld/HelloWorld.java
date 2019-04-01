a33b328442c1b2a1a721e6498aed819f56ad4201
Masikc <47584687+Masikc@users.noreply.github.com>
2019-04-01 21:35:22 +0500

    Update HelloWorld.java

--- src/main/java/HelloWorld/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld/HelloWorld.java
@@ -1,1 +1,13 @@
+package HelloWorld.HelloWorld;
 
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
